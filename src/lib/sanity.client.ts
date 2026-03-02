b39ed481634181260a18b40beff2c842c7c4f12f
Ahmad kaab <155531733+ahmadkaab@users.noreply.github.com>
2026-03-03 01:50:01 +0530

    fix(build): add fallback sanity projectId and dataset strings to unblock vercel zero-config build

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'xcwcc66b';
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-02';
 
 export const client = createClient({
